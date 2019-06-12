e5399dbcfa285ff2a9f5f914d5dd71ccfc5b3892
RonRon <ron.lambojon@gmail.com>
2019-06-13 00:07:20 +0800

    redesigning robofriends

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     // state is something that can change and affect the app
+    // smart components
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     }
+    // console.log('constructor');
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+    // this.setState({ robots: robots });
+    // console.log('componentDidMount');
   }
 
   // random name created like a variable
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value });
-    // console.log(filteredRobots);
   }
   
   render() {
     const filteredRobots  = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     });
+    // console.log('render');
 
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots}/>
-      </div>
-    );
+    if(this.state.robots.length === 0) {
+      return <h1>Loading...</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots}/>
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
