ab6b548754b3af3e29e320dcbfa21d39743969da
RonRon <ron.lambojon@gmail.com>
2019-06-15 01:02:11 +0800

    applying react redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
+
+import { setSearchField } from '../actions.js';
+
+// you can name this whatever you want, but this is the redux standards
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  }
+}
+
+// dispatch is what triggers the action
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
 
 class App extends Component {
   constructor() {
@@ -11,27 +28,23 @@ class App extends Component {
     // state is something that can change and affect the app
     // smart components
     this.state = {
-      robots: [],
-      searchField: ''
+      robots: []
     }
     // console.log('constructor');
   }
 
   componentDidMount() {
+    // console.log(this.props.store.getState());
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ robots: users }));
     // this.setState({ robots: robots });
     // console.log('componentDidMount');
   }
-
-  // random name created like a variable
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value });
-  }
   
   render() {
-    const { robots, searchField } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
 
     const filteredRobots  = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
@@ -43,7 +56,7 @@ class App extends Component {
     (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundry> 
             <CardList robots={filteredRobots}/>
@@ -54,4 +67,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
                   <div>
-                    <App />
+                    <Provider store={store}>
+                      <App />
+                    </Provider>
                   </div>
 , document.getElementById('root'));
 
